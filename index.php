b65752dd1534b8f72688350a34ad0a5939fbba98
Brandon Espinoza <espinozab100@gmail.com>
2015-07-24 18:24:21 -0500

    Home page completed and working on About page | Profile pictures added

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+  <head>
+    <title>Brandon Espinoza | Full Stack Developer</title>
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="CSS/main.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img class="logo" src="images/espinozabrand_logo.png"></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">What I Do</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">What I Do <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Full Services</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Contact Me</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+
+<!-- HOME PAGE -->
+
+    <div class="col-md-12 introPic">
+      <div class="col-md-5 introsquare col-md-offset-5">
+        <h1 class="introH1">HI I AM BRANDON ESPINOZA</h1>
+        <h4 class="introH4">FULL STACK DEVELOPER</h4>
+        <hr>
+      </div>
+    </div>
+
+
+<!-- ABOUT ME  -->
+
+
+    <div class="col-md-12">
+      <div class="col-md-4">
+        <img src="images/profile_pic.png" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <div class="container">
+          <h1>ABOUT ME</h1>
+        </div>
+      </div>
+    </div>
+    <script type="text/javascript" href="bower_components/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" href="main.js"></script>
+  </body>
+</html>
